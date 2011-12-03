63ae474b12840835621925a3a5d9c47883424d49
Valentin Lorentz <progval@gmail.com>
2011-12-03 21:29:28 +0100

    First commit. Only defines constants.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wididit',
+      version='0.1',
+      description='The Wididit social network library',
+      author='Valentin Lorentz',
+      author_email='progval@gmail.com',
+      url='http://python.wididit.net/',
+      packages=['wididit'],
+     )
